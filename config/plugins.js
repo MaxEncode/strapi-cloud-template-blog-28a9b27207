dff1fe9911f21f397744c50775d76af69073adbb
Oguzcan Karakoc <okarakoc.mp@gmail.com>
2025-09-18 00:15:20 +0200

    Add GraphQL plugin configuration

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,11 @@
-module.exports = () => ({});
+module.exports = () => ({
+  graphql: {
+    enabled: true,
+    config: {
+      endpoint: '/graphql',
+      shadowCRUD: true,
+      playgroundAlways: true, // only for dev
+      // other options...
+    },
+  },
+});
