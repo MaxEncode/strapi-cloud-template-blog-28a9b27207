3d7c7b6ae8596838e42f26e2deb4e9889b5e4ec7
Oguzcan Karakoc <okarakoc.mp@gmail.com>
2025-09-18 00:26:24 +0200

    Add GraphQL configuration options

--- config/plugins.js
+++ config/plugins.js
@@ -6,6 +6,10 @@ module.exports = () => ({
       shadowCRUD: true,
       playgroundAlways: true, // only for dev
       // other options...
+      subscriptions: false,
+      maxLimit: -1,
+      apolloServer: {},
+      v4CompatibilityMode: process.env.STRAPI_GRAPHQL_V4_COMPATIBILITY_MODE ?? false,
     },
   },
 });
